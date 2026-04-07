b6ceba205fd7b3be9c1f517871d8b15d5657925f
edkrueger <22670796+edkrueger@users.noreply.github.com>
2026-04-07 17:19:26 -0500

    Roc

--- class_exercises/30-economic_news.py
+++ class_exercises/30-economic_news.py
@@ -1,3 +1,5 @@
+from matplotlib import pyplot as plt
+
 import pandas as pd
 
 from sklearn.metrics import classification_report
@@ -6,6 +8,11 @@ from sklearn.pipeline import make_pipeline
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import BernoulliNB, MultinomialNB
 from sklearn.linear_model import RidgeClassifierCV
+from sklearn.metrics import RocCurveDisplay, PrecisionRecallDisplay
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+
 
 if __name__ == "__main__":
 
@@ -16,7 +23,7 @@ if __name__ == "__main__":
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
-    model = make_pipeline(CountVectorizer(), TfidfTransformer(), RidgeClassifierCV())
+    model = make_pipeline(CountVectorizer(ngram_range=(1,3)), TfidfTransformer(), RandomForestClassifier(n_jobs=-1))
 
     model.fit(X_train, y_train)
 
@@ -24,3 +31,14 @@ if __name__ == "__main__":
     y_hat_test = model.predict(X_test)
 
     print(classification_report(y_true=y_test, y_pred=y_hat_test))
+
+
+    y_score_test = model.predict_proba(X_test)[:, 1]
+
+    print(y_hat_test)
+    print(y_score_test)
+
+    RocCurveDisplay.from_predictions(y_true=y_test, y_score=y_score_test)
+    plt.savefig("roc.png")
+
+
